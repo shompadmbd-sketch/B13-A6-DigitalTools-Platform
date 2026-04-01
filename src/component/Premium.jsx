3a47f66bffe9d3f11f46cdc1d451eb384e1c26a0
shompadmbd-sketch <shompadmbd@gmail.com>
2026-04-01 16:33:14 +0600

    Add UI for premium section layout

---
+++ src/component/Premium.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Premium = () => {
+    return (
+         <div className="mx-auto text-center mt-30 mb-4">
+                <h1 className="text-[#101727] text-[48px] font-extrabold">Premium Digital Tools</h1>
+                <p className="text-[16px] font-normal text-[#627382]">Choose from our curated collection of premium digital products designed <br/> to boost your productivity and creativity.</p>
+            </div>
+    );
+};
+
+export default Premium;
