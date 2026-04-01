4981cdbcb5ce63e991d804a74a0d6eb56e62a7c7
shompadmbd-sketch <shompadmbd@gmail.com>
2026-04-01 20:39:55 +0600

    perform final checks

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -7,9 +7,11 @@ const Banner = () => {
     return (
         <div className="relative min-h-162.5 flex items-center overflow-hidden mt-20 mb-20 sm:mt-15 md:mt-36 lg:mt-10">
 
-            <div className="mx-auto flex flex-col-reverse lg:grid lg:grid-cols-2 gap-5 items-center relative z-10 px-4 md:px-8 lg:px-12 xl:px-[100px] min-[1556px]:px-[250px] w-full max-w-[1920px]">
+            {/* Desktop UI 100% Original Rakha Hoyeche (230px padding preserved) */}
+            <div className="mx-auto flex flex-col-reverse lg:grid lg:grid-cols-2 gap-10 items-center relative z-10 px-4 md:px-8 lg:px-12 xl:px-[230px] min-[1556px]:px-[250px]">
 
-                <div className="space-y-4 text-center lg:text-left flex flex-col items-center lg:items-start w-full">
+                {/* Left Content */}
+                <div className="space-y-4 text-center lg:text-left flex flex-col items-center lg:items-start">
 
                     <div className="inline-flex items-center gap-2 bg-[#E1E7FF] border border-[#E1E7FF] text-sm px-5 py-2 rounded-full">
                         <span><img src={CircleImg} alt="" /></span>
@@ -18,7 +20,8 @@ const Banner = () => {
                         </span>
                     </div>
 
-                    <h1 className="text-4xl lg:text-[60px] min-[1300px]:max-[1555px]:text-[52px] min-[1556px]:text-[40px] font-extrabold leading-[1.1] text-[#101727]">
+                    {/* Desktop e 60px thakbe, Tablet/Mobile e 2-line thakbe */}
+                    <h1 className="text-4xl lg:text-[60px] font-extrabold leading-[1.16] text-[#101727]">
                         Supercharge Your
                         <br className="block lg:hidden" />
                         <span className="">
@@ -48,11 +51,11 @@ const Banner = () => {
                     </div>
                 </div>
 
-                
-                <div className="relative flex justify-center lg:justify-end w-full lg:mt-[-40px] pb-10 lg:pb-0">
-                    <div className="relative w-full max-w-[650px] lg:max-w-none flex justify-center lg:justify-end">
+                {/* Right Image: Margin Adjustment Shudhu Tablet/Mobile er jonno */}
+                <div className="relative flex justify-center lg:justify-end mt-0 lg:mt-0 pb-10 lg:pb-0">
+                    <div className="relative w-full max-w-[500px] lg:max-w-none">
                         <img
-                            className="relative h-auto lg:h-[450px] xl:h-[500px] min-[1556px]:h-130 w-full lg:w-auto object-contain drop-shadow-2xl rounded-3xl animate-pulse"
+                            className="relative h-auto lg:h-130 w-full object-contain drop-shadow-2xl rounded-3xl animate-pulse"
                             src={BannerImage}
                             alt="AI Models Banner"
                         />
